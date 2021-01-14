8a850cd5832a2d03541a25766d9e1b3de50d5637
dkattimu <dkattimu@gmail.com>
2021-01-13 22:01:00 -0500

    more tailwind config..

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+//import '../styles/globals.css'
+import "tailwindcss/tailwind.css" // for direct usage in jsx
+import '../styles/globals.css'   // for global settings
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
