f68aea89377dca7629095a254f59bcf352fc100b
Dodzi <dkattimu@gmail.com>
2024-08-17 21:48:30 -0400

    first candidate ...

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',//['selector', '[data-mode="dark"]'],
 	theme: {
 		extend: {},
+
 	},
 	plugins: [],
 }
