04ad2c5b0f64f7afd82e7ed00215e488007eeb16
Dodzi <dkattimu@gmail.com>
2024-08-24 17:58:35 -0400

    ... theme toggle...

--- src/components/HeaderIcon.tsx
+++ src/components/HeaderIcon.tsx
@@ -1,54 +1,106 @@
 //import type { Icon } from "astro/runtime/client///dev-toolbar/ui-library/icons.js";
 
+import { string } from "astro/zod";
+
 type IconProps = {
-  option: string;
-  descr: string
-  style?:string
-}
+    option?: string;
+    descr: string;
+    style?: string;
+};
 
-let svg_class = "text-green-100 size-6"
+let svg_class = "text-green-100 size-6";
 
-let HeaderIcon = ({option, descr, style=svg_class}: IconProps) =>{ 
-    if (option=="home"){
-    return (
-      <div className="flex"> 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={style}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z"/>
-        </svg> {descr}
-      </div> 
-      )
-    } else if (option=="blog"){
-      return(
-        <div className="flex">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={style}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
-          </svg>{descr}
-          </div>
-      )
+let HeaderIcon = ({ option, descr, style = svg_class }: IconProps) => {
+    if (option == "home") {
+        return (
+            <div className="flex">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className={style}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z"
+                    />
+                </svg>{" "}
+                {descr}
+            </div>
+        );
+    } else if (option == "blog") {
+        return (
+            <div className="flex">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className={style}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+                    />
+                </svg>
+                {descr}
+            </div>
+        );
     } else {
-      return(
-        <div className="flex ">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={style}>
-          <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
-        </svg>{descr}
-          </div>
-      )
+        return (
+            <div className="flex ">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className={style}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+                    />
+                </svg>
+                {descr}
+            </div>
+        );
     }
+};
+
+interface ThemeToggleProps {
+    descr: string;
+    style?: string;
+    handler?: () => void;
 }
 
-let ThemeToggle = ({descr="Dark/Light THeme", style=svg_class}: IconProps) =>{
-  return (
-    <div>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-    </svg> {descr}
-  </div>
+let descr_ = "Dark/Light Theme";
+let style_ = "bg-blue-700  px-4 pb-1 mb-2 rounded-full";
 
-  )
-}
+let ThemeToggle = ({
+    descr = descr_,
+    style = style_,
+    handler,
+}: ThemeToggleProps) => {
+    let toggleHandler = () => {
+        alert("TEST");
+    };
+    return (
+        <div className="mb-0">
+            <button className={style} onClick={toggleHandler}>
+                {descr}
+            </button>
+        </div>
+    );
+};
 
-export default HeaderIcon;
-
+export { ThemeToggle, HeaderIcon };
 
 /*
   <>
